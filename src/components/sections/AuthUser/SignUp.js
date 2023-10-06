7be796db7e6aa13c6a04f63c5ff1cb948df39a2c
Sami-AL-njadat <sami.alnajadat@gmail.com>
2023-10-06 16:16:45 +0300

    sam

--- src/components/sections/AuthUser/SignUp.js
+++ src/components/sections/AuthUser/SignUp.js
@@ -128,16 +128,19 @@
 import React from "react";
 import axios from "axios";
 import { useHistory } from "react-router-dom";
-
+import validation from './Validation'
 
 function SignUpForm({ setIsLoggedIn }) {
-      const history = useHistory();
+  const history = useHistory();
 
   const [state, setState] = React.useState({
     name: "",
     email: "",
     password: "",
   });
+
+    const [errors, setErrors] = React.useState({});
+
 
   const handleChange = (evt) => {
     const value = evt.target.value;
@@ -146,18 +149,33 @@ function SignUpForm({ setIsLoggedIn }) {
       [evt.target.name]: value,
     });
   };
+
+
+
   const handleOnSubmit = async (evt) => {
     evt.preventDefault();
+
+    // function  (evt) {
+    //   evt.preventDefault();
+    //   setErrors(validation());
+    // }
+
+       const validationErrors = validation(state); // Validate the form fields
+       if (Object.keys(validationErrors).length > 0) {
+         // If there are validation errors, set them in the state
+         setErrors(validationErrors);
+         return;
+       } 
 
     const response = await axios.post(
       "https://651be95a194f77f2a5af127c.mockapi.io/users",
       state
     );
 
-    // alert(`User registered successfully with ID: ${response.data.id}`);
-    
+    alert(`User registered successfully  ${response.data.name}`);
+
     setIsLoggedIn = true;
-    
+
     setState({
       name: "",
       email: "",
@@ -170,17 +188,7 @@ function SignUpForm({ setIsLoggedIn }) {
     <div className="form-containers sign-up-containers">
       <form className="forms" onSubmit={handleOnSubmit}>
         <h3 className="h3">Create Account</h3>
-        {/* <div className="social-container">
-          <a href="#" className="social">
-            <i className="fab fa-facebook-f" />
-          </a>
-          <a href="#" className="social">
-            <i className="fab fa-google-plus-g" />
-          </a>
-          <a href="#" className="social">
-            <i className="fab fa-linkedin-in" />
-          </a>
-        </div> */}
+
         <span>or use your email for registration</span>
         <input
           type="text"
@@ -188,27 +196,32 @@ function SignUpForm({ setIsLoggedIn }) {
           value={state.name}
           onChange={handleChange}
           placeholder="Name"
-          required
+          // required
           className="inputs"
         />
+        {errors.name && <p style={{ color: "red" }}>{errors.name}</p>}
         <input
           type="email"
           name="email"
           value={state.email}
           onChange={handleChange}
           placeholder="Email"
-          required
+          // required
           className="inputs"
         />
+        {errors.email && <p style={{ color: "red" }}>{errors.email}</p>}
+
         <input
           type="password"
           name="password"
           value={state.password}
           onChange={handleChange}
           placeholder="Password"
-          required
+          // required
           className="inputs"
         />
+        {errors.password && <p style={{ color: "red" }}>{errors.password}</p>}
+
         <button className="buttons">Sign Up</button>
       </form>
     </div>
