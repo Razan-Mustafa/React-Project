e3c938744be778ff84b8bfe70a4678c1f797d058
Sami-AL-njadat <sami.alnajadat@gmail.com>
2023-10-06 22:29:08 +0300

    sam

--- src/components/sections/AuthUser/SignIn.js
+++ src/components/sections/AuthUser/SignIn.js
@@ -1,23 +1,133 @@
-import React from "react";
+// import React from "react";
+// import axios from "axios";
+// import { useHistory } from "react-router-dom";
+// import Swal from "sweetalert2";
+
+
+
+// function SignInForm({ setIsLoggedIn }) {
+//   const history = useHistory();
+
+//   const [state, setState] = React.useState({
+//     email: "",
+//     password: "",
+//   });
+
+//   const handleChange = (evt) => {
+//     const value = evt.target.value;
+//     setState({
+//       ...state,
+//       [evt.target.name]: value,
+//     });
+//   };
+
+//   const handleOnSubmit = async (evt) => {
+//     evt.preventDefault();
+
+//     const { email, password } = state;
+
+//     try {
+//       const response = await axios.get(
+//         `https://651be95a194f77f2a5af127c.mockapi.io/users?email=${email}&password=${password}`
+//       );
+
+
+// //  const user = response.data.find(
+// //    (user) => user.email === state.email && user.password === state.password
+// //  );
+//  const user = response.data.find (
+//   user => user.email === state.email && user.password === state.password
+//  );
+//   //  const user = data.find((user) => user.email === loginData.email && user.password === loginData.password);
+
+     
+           
+//       if (response.data.length > 0) {
+//         alert("Login successful!");
+
+        
+//         setIsLoggedIn = true;
+//         ;
+//         sessionStorage.setItem("userId", user[0].id);
+//         sessionStorage.setItem("userName", user[0].name);
+//         sessionStorage.setItem("IsLoggedIn", setIsLoggedIn);
+//         history.goBack();
+
+//         console.log(sessionStorage.getItem("userId"));
+//       } else {
+//         Swal.fire({
+//           title: 'Login failed. Please check your credentials.',
+//           customClass: {
+//             confirmButton: 'custom-confirm-button-class'
+//           }
+//         })
+//         // alert("Login failed. Please check your credentials.");
+//       }
+//     } 
+//     catch (error) {
+//       alert("Login failed.");
+//     }
+
+
+
+
+//     setState({
+//       email: "",
+//       password: "",
+//     });
+
+
+//   };
+
+//   return (
+//     <div className="form-containers sign-in-containers">
+//       <form className="forms" onSubmit={handleOnSubmit}>
+//         <h3 className="h3">Sign in</h3>
+
+//         <span className="span"> use your account</span>
+//         <input
+//           type="email"
+//           placeholder="Email"
+//           name="email"
+//           value={state.email}
+//           onChange={handleChange}
+//           className="inputs"
+//         />
+//         <input
+//           type="password"
+//           name="password"
+//           placeholder="Password"
+//           value={state.password}
+//           onChange={handleChange}
+//           className="inputs"
+//         />
+//         <a href="#">Forgot your password?</a>
+//         <button className="buttons">Sign In</button>
+//       </form>
+//     </div>
+//   );
+// }
+
+// export default SignInForm;
+import React, { useState } from "react";
 import axios from "axios";
 import { useHistory } from "react-router-dom";
 import Swal from "sweetalert2";
 
-
-
 function SignInForm({ setIsLoggedIn }) {
   const history = useHistory();
 
-  const [state, setState] = React.useState({
+  const [state, setState] = useState({
     email: "",
     password: "",
   });
+   setIsLoggedIn = true;
 
   const handleChange = (evt) => {
-    const value = evt.target.value;
+    const { name, value } = evt.target;
     setState({
       ...state,
-      [evt.target.name]: value,
+      [name]: value,
     });
   };
 
@@ -27,43 +137,59 @@ function SignInForm({ setIsLoggedIn }) {
     const { email, password } = state;
 
     try {
-      const response = await axios.get(
-        `https://651be95a194f77f2a5af127c.mockapi.io/users?email=${email}&password=${password}`
-      );
-      const user = response.data;
-      // try{
-      if (response.data.length > 0) {
-        // alert("Login successful!");
-        setIsLoggedIn = true;
-        ;
-        sessionStorage.setItem("userId", user[0].id);
-        sessionStorage.setItem("userName", user[0].name);
-        sessionStorage.setItem("IsLoggedIn", setIsLoggedIn);
-        history.goBack();
+    const response = await axios.get(
+      `https://651be95a194f77f2a5af127c.mockapi.io/users?email=${email}&password=${password}`
+    );
 
-        console.log(sessionStorage.getItem("userId"));
-      } else {
+    const user = response.data.find(
+      (user) => user.email === state.email && user.password === state.password
+    );
+
+    if (user) {
+      // alert("Login successful!");
+
+     
+
+      // setIsLoggedIn(true); // Update state with true, not assignment
+      sessionStorage.setItem("userId", user.id);
+      sessionStorage.setItem("userName", user.name);
+      sessionStorage.setItem("IsLoggedIn", "true"); // Store as string
         Swal.fire({
-          title: 'Login failed. Please check your credentials.',
+          title: "Login successful!",
           customClass: {
-            confirmButton: 'custom-confirm-button-class'
-          }
-        })
-        // alert("Login failed. Please check your credentials.");
-      }
-    } catch (error) {
-      alert("Login failed.");
+            confirmButton: "custom-confirm-button-class",
+          },
+        });
+      history.goBack();
+
+      console.log(sessionStorage.getItem("userId"));
+    } else {
+      Swal.fire({
+        title: "Login failed. Please check your email & password.",
+        customClass: {
+          confirmButton: "custom-confirm-button-class",
+        },
+      });
     }
+    } catch (error) {
+      // alert("Login failed.");
+      Swal.fire({
+        title: "Login failed.",
+        customClass: {
+          confirmButton: "custom-confirm-button-class",
+        },
+      });
 
 
 
+
+      console.log(error);
+    }
 
     setState({
       email: "",
       password: "",
     });
-
-
   };
 
   return (
@@ -88,8 +214,10 @@ function SignInForm({ setIsLoggedIn }) {
           onChange={handleChange}
           className="inputs"
         />
-        <a href="#">Forgot your password?</a>
-        <button className="buttons">Sign In</button>
+        <a>Forgot your password?</a>
+        <button type="submit" className="buttons">
+          Sign In
+        </button>
       </form>
     </div>
   );
--- src/helper/Contacthelper.js
+++ src/helper/Contacthelper.js
@@ -1,78 +1,72 @@
-import React, { Component, Fragment } from 'react';
+import React, { useState } from "react";
+import axios from "axios";
 
-class Contacthelper extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: '',
-            phone: '',
-            email: '',
-            subject: '',
-            message: '',
-            isVerified: false
+function Contacthelper() {
+  const [formData, setFormData] = useState({
+    name: "",
+    phone: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+
+  const [isVerified, setIsVerified] = useState(false);
+
+  const onInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  // REcaptcha
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios
+      .post("https://651a62f5340309952f0d3231.mockapi.io/users", formData)
+      .then((response) => {
+        if (response.data.id !== "") {
+          // Handle success
+          console.log("Request succeeded:", response.data);
+          setIsVerified(true);
+          resetForm();
+        } else {
+          // Handle failure
+          console.error("Request failed:", response.data);
         }
-        this.onNameChange = this.onNameChange.bind(this);
-        this.onPhoneChange = this.onPhoneChange.bind(this);
-        this.onEmailChange = this.onEmailChange.bind(this);
-        this.onSubjectChange = this.onSubjectChange.bind(this);
-        this.onMessageChange = this.onMessageChange.bind(this);
-        this.reCaptchaLoaded = this.reCaptchaLoaded.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-    onNameChange(event) { 
-        this.setState({ name: event.target.value })
-    }
-    onPhoneChange(event) {
-        this.setState({ phone: event.target.value })
-    }
-    onEmailChange(event) {
-        this.setState({ email: event.target.value })
-    }
-    onSubjectChange(event) {
-        this.setState({ subject: event.target.value })
-    }
-    onMessageChange(event) {
-        this.setState({ message: event.target.value })
-    }
-    // REcaptcha
-    reCaptchaLoaded(value) {
-        console.log("Captcha Successfully Loaded", value);
-    }
-    handleSubmit(e) {
-        e.preventDefault();
-        fetch("https://64db19bd593f57e435b0704a.mockapi.io/messages", {
-          method: "POST",
-          body: JSON.stringify(this.state),
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
-        })
-          .then((response) => response.json())
-          .then((response) => {
-            if (response.id !== "") {
-              document
-                .getElementById("server_response_success")
-                .classList.add("d-block");
-              this.resetForm();
-              this.setState({
-                isVerified: true,
-              });
-            } else {
-              document
-                .getElementById("server_response_danger")
-                .classList.add("d-block");
-            }
-          });
-    }
-    resetForm() {
-        this.setState({ name: "", phone: "", email: "", subject: "", message: "", })
-    }
-    render() {
-        return (
-            <Fragment />
-        );
-    }
+      })
+      .catch((error) => {
+        console.error("Request error:", error);
+      });
+  };
+
+  const resetForm = () => {
+    setFormData({
+      name: "",
+      phone: "",
+      email: "",
+      subject: "",
+      message: "",
+    });
+  };
+
+  return (
+    <div>
+      {/* Your form JSX here */}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={onInputChange}
+          placeholder="Name"
+        />
+        {/* Add other form fields */}
+        <button type="submit">Submit</button>
+      </form>
+      {isVerified && <div id="server_response_success">Success</div>}
+    </div>
+  );
 }
 
 export default Contacthelper;
